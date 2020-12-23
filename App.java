d1d33481632153d5380b2faa0517587c05051539
Adenita <nittaluta@gmail.com>
2020-12-23 21:48:04 +0100

    added some methodsin minStudenView and app

--- App.java
+++ App.java
@@ -1,7 +1,15 @@
-import View.MainView;
+//import View.MainView;
+import View.StudentsView.MainStudentView;
 
 public class App {
     public static void main(String[] args) {
-        MainView.menu();
+       // MainView.menu();
+       MainStudentView.AddDepartament("Matematike");
+       MainStudentView.AddProgram("Matematika", 1000);
+       MainStudentView.AddStudent("bledi", "buduri", 0, 1);
+       MainStudentView.AddStudent("adenita", "luta", 0, 1);
+       MainStudentView.ViewAllStudents();
+       
+
     }
 }
