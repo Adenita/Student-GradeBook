76dcd5986df9c59d5d48a6357925fa1e8fa59214
Adenita <48224949+Adenita@users.noreply.github.com>
2022-03-07 01:58:53 +0100

    Update Database.java

--- DataAccess/Database.java
+++ DataAccess/Database.java
@@ -3,20 +3,16 @@ package DataAccess;
 import java.util.ArrayList;
 import java.util.List;
 
-import Model.Subject;
-import Model.Departament;
+
 import Model.Grade;
 import Model.Profesor;
 import Model.Student;
-import Model.Program;
+import Model.Subject;
 
 public class Database 
 {
-    public static List<Subject> subjects = new ArrayList<>();
+	public static List<Subject> subjects = new ArrayList<>();
     public static List<Grade> grades = new ArrayList<>();
     public static List<Profesor> profesors = new ArrayList<>();
     public static List<Student> students = new ArrayList<>();
-    public static List<Program> programs = new ArrayList<>();
-	public static List<Departament> departaments = new ArrayList<>();
-
 }
