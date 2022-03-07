8f2a63edfce969cb40be8c35b201a04d8b18d7f1
Adenita <48224949+Adenita@users.noreply.github.com>
2022-03-07 02:10:11 +0100

    Update App.java

--- App.java
+++ App.java
@@ -3,13 +3,6 @@ import View.StudentsView.MainStudentView;
 
 public class App {
     public static void main(String[] args) {
-       // MainView.menu();
-       MainStudentView.AddDepartament("Matematike");
-       MainStudentView.AddProgram("Matematika", 1000);
-       MainStudentView.AddStudent("bledi", "buduri", 0, 1);
-       MainStudentView.AddStudent("adenita", "luta", 0, 1);
-       MainStudentView.ViewAllStudents();
-       
-
+       MainView.menu();    
     }
 }
